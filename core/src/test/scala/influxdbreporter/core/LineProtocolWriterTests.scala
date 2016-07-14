595f3c6aed282ff6317d47a497bb0f638b1be6fb
Mateusz Kołodziejczyk <mkl@touk.pl>
2016-07-14 14:22:34 +0200

    fixed: unescaped '=' char in line protocol payload + better escaping

--- core/src/test/scala/influxdbreporter/core/LineProtocolWriterTests.scala
+++ core/src/test/scala/influxdbreporter/core/LineProtocolWriterTests.scala
@@ -52,24 +52,6 @@ class LineProtocolWriterTests extends WordSpec {
       }
     }
 
-    "properly escape space character" in {
-      assertResult(WriterData[String]("measurement\\ 1,t\\ 2=tv\\ 2 f\\ 1=\"fv\\ 1\" 1000000\n")) {
-        lineProtocolWriter.write("measurement 1", Field("f 1", "fv 1"), Tag("t 2", "tv 2"), 1000000L)
-      }
-    }
-
-    "properly escape comma character" in {
-      assertResult(WriterData[String]("measurement\\,1,t\\,2=tv\\,2 f\\,1=\"fv\\,1\" 1000000\n")) {
-        lineProtocolWriter.write("measurement,1", Field("f,1", "fv,1"), Tag("t,2", "tv,2"), 1000000L)
-      }
-    }
-
-    "properly escape quotation mark character in field string" in {
-      assertResult(WriterData[String]("measurement\"1,t\"2=tv\"2 f\"1=\"fv\\\"1\" 1000000\n")) {
-        lineProtocolWriter.write("measurement\"1", Field("f\"1", "fv\"1"), Tag("t\"2", "tv\"2"), 1000000L)
-      }
-    }
-
     "generate data with sorted tags" in {
       assertResult(WriterData[String]("measurement,a=2,b=2,c=2,d=2 f=1i 1000000\n")) {
         lineProtocolWriter.write("measurement", Field("f", 1), Tag("b", 2) :: Tag("d", 2) :: Tag("a", 2) :: Tag("c", 2) :: Nil , 1000000L)
@@ -80,6 +62,144 @@ class LineProtocolWriterTests extends WordSpec {
       assertResult(WriterData[String]("measurement,a=a1,b=b1,t=2 f=1i 1000000\n")) {
         new LineProtocolWriter(Tag("a", "a1") :: Tag("b", "b1") :: Nil)
           .write("measurement", Field("f", 1), Tag("t", 2), 1000000L)
+      }
+    }
+
+    "escape given characters in measurement name" when {
+
+      "char is ' '" in {
+        assertResult(WriterData[String]("measurement\\ 1,a=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement 1", Field("f", 1), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "char is ','" in {
+        assertResult(WriterData[String]("measurement\\,1,t=1 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement,1", Field("f", 1), Tag("t", 1), 1000000L)
+        }
+      }
+    }
+
+    "escape given characters in tag name" when {
+
+      "char is '='" in {
+        assertResult(WriterData[String]("measurement,a\\=c=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("a=c", 2), 1000000L)
+        }
+      }
+
+      "char is ' '" in {
+        assertResult(WriterData[String]("measurement,tag\\ a=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("tag a", 2), 1000000L)
+        }
+      }
+
+      "char is ','" in {
+        assertResult(WriterData[String]("measurement,t\\,a=1 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("t,a", 1), 1000000L)
+        }
+      }
+    }
+
+    "escape given characters in tag value" when {
+
+      "char is '='" in {
+        assertResult(WriterData[String]("measurement,a=a\\=1 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("a", "a=1"), 1000000L)
+        }
+      }
+
+      "char is ' '" in {
+        assertResult(WriterData[String]("measurement,a=val\\ 2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("a", "val 2"), 1000000L)
+        }
+      }
+
+      "char is ','" in {
+        assertResult(WriterData[String]("measurement,t=2\\,a f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", 1), Tag("t", "2,a"), 1000000L)
+        }
+      }
+    }
+
+    "escape given characters in field name" when {
+
+      "char is '='" in {
+        assertResult(WriterData[String]("measurement,a=1 f\\=a=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f=a", 1), Tag("a", 1), 1000000L)
+        }
+      }
+
+      "char is ' '" in {
+        assertResult(WriterData[String]("measurement,a=2 f\\ 1=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f 1", 1), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "char is ','" in {
+        assertResult(WriterData[String]("measurement,t=1 f\\,a=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f,a", 1), Tag("t", 1), 1000000L)
+        }
+      }
+    }
+
+    "escape given characters in field value" when {
+
+      "char is '\"'" in {
+        assertResult(WriterData[String]("measurement,t=1 f=\"a\\\"b\" 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", "a\"b"), Tag("t", 1), 1000000L)
+        }
+      }
+    }
+
+    "not escape" when {
+
+      "'=' is in measurement name" in {
+        assertResult(WriterData[String]("measurement=1,a=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement=1", Field("f", 1), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "'=' is in field value" in {
+        assertResult(WriterData[String]("measurement,a=1 f=\"val 1\" 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", "val 1"), Tag("a", 1), 1000000L)
+        }
+      }
+
+      "space is is field value" in {
+        assertResult(WriterData[String]("measurement,a=2 f=\"1 a\" 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", "1 a"), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "',' is in field value" in {
+        assertResult(WriterData[String]("measurement,a=2 f=\"1,a\" 1000000\n")) {
+          lineProtocolWriter.write("measurement", Field("f", "1,a"), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "'\"' in measurement name" in {
+        assertResult(WriterData[String]("measurement\"1,a=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement\"1", Field("f", 1), Tag("a", 2), 1000000L)
+        }
+      }
+
+      "'\"' in tag name" in {
+        assertResult(WriterData[String]("measurement1,a\"1=2 f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement1", Field("f", 1), Tag("a\"1", 2), 1000000L)
+        }
+      }
+
+      "'\"' in tag value" in {
+        assertResult(WriterData[String]("measurement1,a=2\"a f=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement1", Field("f", 1), Tag("a", "2\"a"), 1000000L)
+        }
+      }
+
+      "'\"' in field name" in {
+        assertResult(WriterData[String]("measurement1,a=2 f\"a=1i 1000000\n")) {
+          lineProtocolWriter.write("measurement1", Field("f\"a", 1), Tag("a", 2), 1000000L)
+        }
       }
     }
   }
